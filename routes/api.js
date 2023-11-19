e799c67773f0ee5df57db87fc6734f534e78853b
Odong Martin <odongmartin21@gmail.com>
2023-11-19 16:55:31 +0300

    api key nolonger public property

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const https = require('https');
+require('dotenv').config();
+
+const apiKey = process.env.APIKEY;
 
 router.get('/github/userinfo/:user', async function(req, res) {
     const user = req.params.user;
@@ -8,9 +11,9 @@ router.get('/github/userinfo/:user', async function(req, res) {
         hostname: 'api.github.com',
         path: '/users/'+ user,
         headers: {
-            'User-Agent': 'OdongMartin', //'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+            'User-Agent': 'OdongMartin',
         },
-        OAuth: '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',
+        OAuth: apiKey,
     }
     https.get(options, function(apiResponse) {
         apiResponse.pipe(res);
