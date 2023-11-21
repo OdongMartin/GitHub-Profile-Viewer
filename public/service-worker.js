7f7b231e57497835c7e758c5ab189393ac75a8e8
Odong Martin <odongmartin21@gmail.com>
2023-11-22 01:06:51 +0300

    changes to manifest json file

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,19 +7,19 @@ const urlsToCache = [
   // Add other static assets to cache
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(function(cache) {
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(function(response) {
         return response || fetch(event.request);
       })
   );
